9baf86e7bbc0e65a96d6ff93a7240a8a78b9c699
syedkasghar <syedkasghar@gmail.com>
2023-08-05 19:49:53 -0500

    2 files modified, 2 added

--- src/app/_helpers/index.ts
+++ src/app/_helpers/index.ts
@@ -1,2 +1,3 @@
 export * from './fake-backend';
 export * from './must-match.validator';
+export * from './auth.guard';
--- src/app/_services/index.ts
+++ src/app/_services/index.ts
@@ -2,3 +2,4 @@ export * from './device.service';
 export * from './incident.service';
 export * from './user.service';
 export * from './alert.service';
+export * from './authentication.service';
